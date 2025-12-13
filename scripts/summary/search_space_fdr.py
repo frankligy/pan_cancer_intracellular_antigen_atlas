22a5929c6190a139f6be3f1c75da7183822cfead
Guangyuan(Frank) Li <59619831+frankligy@users.noreply.github.com>
2025-12-13 13:39:03 -0500

    Update search_space_fdr.py

--- scripts/summary/search_space_fdr.py
+++ scripts/summary/search_space_fdr.py
@@ -124,51 +124,121 @@ immuno_dir = '/gpfs/data/yarmarkovichlab/Frank/pan_cancer/immunopeptidome'
 iedb = pd.read_csv('all_epitope_no_b_human_linear_mhc_i.tsv',sep='\t')
 iedb_peptides = set(iedb['Epitope - Name'].unique())
 
-c = 'BRCA'
+cancers = ['BRCA','KIRC']
 
 # search_space
-space_dict = {}
-combined_fasta = os.path.join(immuno_dir,cancers2immuno[c],'combined_{}_pan_cancer.fasta'.format(c))
-with open(combined_fasta,'r') as in_handle:
-    for title,seq in SimpleFastaParser(in_handle):
-        identity = classify_source(title)
-        if identity in space_dict.keys():
-            space_dict[identity] += len(seq)
-        else:
-            space_dict[identity] = 0
+cats = ['contaminant','variant','pathogen','fusion','intron_retention','nuORF','TE_chimeric_transcript','splicing','ERV','unknown','nc_isoform','self_gene']
+data = np.empty((len(cancers),len(cats)),dtype=np.float64)
+for i,c in enumerate(cancers):
+    space_dict = {}
+    combined_fasta = os.path.join(immuno_dir,cancers2immuno[c],'combined_{}_pan_cancer.fasta'.format(c))
+    with open(combined_fasta,'r') as in_handle:
+        for title,seq in SimpleFastaParser(in_handle):
+            identity = classify_source(title)
+            if identity in space_dict.keys():
+                space_dict[identity] += len(seq)
+            else:
+                space_dict[identity] = 0
+                space_dict[identity] += len(seq)
 
-# maxquant fdr iedb and 70
-msms1 = pd.read_csv(os.path.join(root_atlas_dir,c,'antigen','0.01','msmsScans_all_add_tesorai.txt'),sep='\t')
-msms1 = msms1.loc[msms1['Identified_vanilla']==True,:]
+    for cat in cats:
+        if cat not in space_dict.keys():
+            space_dict[cat] = 0
+    data[i,:] = pd.Series(space_dict).loc[cats].values
 
-msms5 = pd.read_csv(os.path.join(root_atlas_dir,c,'antigen','0.05','msmsScans_all_add_tesorai.txt'),sep='\t')
-msms5 = msms5.loc[msms5['Identified_vanilla']==True,:]
+df = pd.DataFrame(data=data,index=cancers,columns=cats)
+df['tmp'] = df['nc_isoform'] + df['self_gene']
+df['self_gene'] = df['tmp']
+df.drop(columns=['unknown','nc_isoform','tmp'],inplace=True)
+increase_by = df.iloc[:,:9].sum(axis=1) / df.iloc[:,9]
 
-only_in_5 = set(msms5['Sequence']).difference(set(msms1['Sequence']))
+data = df.values
+fig,ax = plt.subplots()
+ax.bar(cancers,data[:,0],label='contaminant')
+ax.bar(cancers,data[:,1],bottom=data[:,0],label='variant')
+ax.bar(cancers,data[:,2],bottom=data[:,:2].sum(axis=1),label='pathogen')
+ax.bar(cancers,data[:,3],bottom=data[:,:3].sum(axis=1),label='fusion')
+ax.bar(cancers,data[:,4],bottom=data[:,:4].sum(axis=1),label='intron_retention')
+ax.bar(cancers,data[:,5],bottom=data[:,:5].sum(axis=1),label='nuORF')
+ax.bar(cancers,data[:,6],bottom=data[:,:6].sum(axis=1),label='TE_chimeric_transcript')
+ax.bar(cancers,data[:,7],bottom=data[:,:7].sum(axis=1),label='splicing')
+ax.bar(cancers,data[:,8],bottom=data[:,:8].sum(axis=1),label='self_translating_TE')
+ax.bar(cancers,data[:,9],bottom=data[:,:9].sum(axis=1),label='self_gene')
+for i,c in enumerate(cancers):
+    ax.text(x=i,y=data[:,:10].sum(axis=1)[i],s=round(increase_by.iloc[i],2))
+ax.set_ylabel('value')
+# ax.set_ylim([0,1.05])
+ax.set_xticklabels(cancers,rotation=60)
+ax.legend()
+plt.savefig('search_space_total.pdf',bbox_inches='tight')
+plt.close()
 
-iedb_common = iedb_peptides.intersection(only_in_5)
-print(len(iedb_common))
-seventy1 = msms1.loc[msms1['Score']>70,:]
-seventy5 = msms5.loc[msms5['Score']>70,:]
-prop_seventy1 = seventy1.shape[0] / msms1.shape[0]
-prop_seventy5 = seventy5.shape[0] / msms5.shape[0]
-print(prop_seventy1,prop_seventy5)
+# 70 and iedb
+# data_list = []
+# for c in cancers:
+#     msms1 = pd.read_csv(os.path.join(root_atlas_dir,c,'antigen','0.01','msmsScans_all_add_tesorai.txt'),sep='\t')
+#     msms5 = pd.read_csv(os.path.join(root_atlas_dir,c,'antigen','0.05','msmsScans_all_add_tesorai.txt'),sep='\t')
+#     # maxquant fdr iedb and 70
+#     msms1_ = msms1.loc[msms1['Identified_vanilla']==True,:]
+#     msms5_ = msms5.loc[msms5['Identified_vanilla']==True,:]
+#     only_in_5 = set(msms5_['Sequence']).difference(set(msms1_['Sequence']))
+#     iedb_common = iedb_peptides.intersection(only_in_5)
+#     seventy1 = msms1_.loc[msms1_['Score']>70,:]
+#     seventy5 = msms5_.loc[msms5_['Score']>70,:]
+#     prop_seventy1 = seventy1.shape[0] / msms1_.shape[0]
+#     prop_seventy5 = seventy5.shape[0] / msms5_.shape[0]
+#     data_m = [len(iedb_common),prop_seventy1,prop_seventy5]
 
-# rescore fdr iedb and 70
-msms1 = pd.read_csv(os.path.join(root_atlas_dir,c,'antigen','0.01','msmsScans_all_add_tesorai.txt'),sep='\t')
-msms1 = msms1.loc[msms1['Identified_rescore']==True,:]
+#     # rescore fdr iedb and 70
+#     msms1_ = msms1.loc[msms1['Identified_rescore']==True,:]
+#     msms5_ = msms5.loc[msms5['Identified_rescore']==True,:]
+#     only_in_5 = set(msms5_['Sequence']).difference(set(msms1_['Sequence']))
+#     iedb_common = iedb_peptides.intersection(only_in_5)
+#     seventy1 = msms1_.loc[msms1_['Score']>70,:]
+#     seventy5 = msms5_.loc[msms5_['Score']>70,:]
+#     prop_seventy1 = seventy1.shape[0] / msms1_.shape[0]
+#     prop_seventy5 = seventy5.shape[0] / msms5_.shape[0]
+#     data_r = [len(iedb_common),prop_seventy1,prop_seventy5]
 
-msms5 = pd.read_csv(os.path.join(root_atlas_dir,c,'antigen','0.05','msmsScans_all_add_tesorai.txt'),sep='\t')
-msms5 = msms5.loc[msms5['Identified_rescore']==True,:]
+#     data_list.append(data_m+data_r)
 
-only_in_5 = set(msms5['Sequence']).difference(set(msms1['Sequence']))
+# df = pd.DataFrame.from_records(data=data_list,index=cancers,columns=['maxquant_iedb_only_5','maxquant_hc_1','maxquant_hc_5','rescore_iedb_only_5','rescore_hc_1','rescore_hc_5'])
+# df1 = df.loc[:,['maxquant_hc_1','maxquant_hc_5']]
+# df1.plot.bar(rot=0)
+# plt.savefig('maxquant_hc.pdf',bbox_inches='tight')
+# plt.close()
 
-iedb_common = iedb_peptides.intersection(only_in_5)
-print(len(iedb_common))
-seventy1 = msms1.loc[msms1['Score']>70,:]
-seventy5 = msms5.loc[msms5['Score']>70,:]
-prop_seventy1 = seventy1.shape[0] / msms1.shape[0]
-prop_seventy5 = seventy5.shape[0] / msms5.shape[0]
-print(prop_seventy1,prop_seventy5)
+# df1 = df.loc[:,['rescore_hc_1','rescore_hc_5']]
+# df1.plot.bar(rot=0)
+# plt.savefig('rescore_hc.pdf',bbox_inches='tight')
+# plt.close()
+
+# df1 = df.loc[:,['maxquant_iedb_only_5','rescore_iedb_only_5']]
+# df1.plot.bar(rot=0)
+# plt.savefig('iedb_miss.pdf',bbox_inches='tight')
+# plt.close()
+
+# anchor
+data = []
+anchors = {
+    'KIRC':['ATFLGSLTGK', 'KLIAGLIFLK', 'DLSRRDVSL']
+}
+
+for k,vs in anchors.items():
+    msms5 = pd.read_csv(os.path.join(root_atlas_dir,k,'antigen','0.05','msmsScans_all_add_tesorai.txt'),sep='\t')
+    for v in vs:
+        sub = msms5.loc[(msms5['Sequence']==v) & (msms5['Identified']=='+'),:]
+        # maxquant range
+        sub2 = sub.loc[sub['Identified_vanilla']==True,:]
+        min_m = sub2['qval_vanilla'].min()
+        max_m = sub2['qval_vanilla'].max()
+        # rescore range
+        sub2 = sub.loc[sub['Identified_rescore']==True,:]
+        min_r = sub2['qval_rescore'].min()
+        max_r = sub2['qval_rescore'].max()
+        data.append((k,v,min_m,max_m,min_r,max_r))
+df = pd.DataFrame(data=data,columns=['cancer','antigen','maxquant_qval_min','maxquant_qval_max','rescore_qval_min','rescore_qval_max'])
+df.to_csv('anchor_qval_range.txt',sep='\t',index=None)
+
 
 
