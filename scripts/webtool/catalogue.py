c73e40ec2baef8ff7a4cc3a7668cb31b524602c9
Guangyuan(Frank) Li <59619831+frankligy@users.noreply.github.com>
2026-09-20 11:42:53 -0400

    Update catalogue.py

--- scripts/webtool/catalogue.py
+++ scripts/webtool/catalogue.py
@@ -213,44 +213,45 @@ cancers2immuno = {
 
 # molecular_catalogue, I'll manually add mutation
 variant_dir = '/gpfs/data/yarmarkovichlab/Frank/pan_cancer/variants'
-variant_dic = {
-    'BRCA':['TCGA-BRCA.mutect2_snv.tsv','gProfiler_hsapiens_brca.csv'],
-    'KIRC':['TCGA-KIRC.mutect2_snv.tsv','gProfiler_hsapiens_kirc.csv'],
-    'COAD':['TCGA-COAD.mutect2_snv.tsv','gProfiler_hsapiens_coad.csv'],
-    'STAD':['TCGA-STAD.somaticmutation_wxs.tsv','gProfiler_hsapiens_stad.csv'],
-    'MESO':['TCGA-MESO.mutect2_snv.tsv','gProfiler_hsapiens_meso.csv'],
-    'LIHC':['TCGA-LIHC.mutect2_snv.tsv','gProfiler_hsapiens_lihc.csv'],
-    'ESCA':['TCGA-ESCA.somaticmutation_wxs.tsv','gProfiler_hsapiens_esca.csv'],
-    'CESC':['TCGA-CESC.mutect2_snv.tsv','gProfiler_hsapiens_cesc.csv'],
-    'BLCA':['TCGA-BLCA.somaticmutation_wxs.tsv','gProfiler_hsapiens_blca.csv'],
-    'RT':[None,None],
-    'AML':['TCGA-LAML.mutect2_snv.tsv','gProfiler_hsapiens_aml.csv'],
-    'DLBC':['TCGA-DLBC.mutect2_snv.tsv','gProfiler_hsapiens_dlbc.csv'],
-    'GBM':['TCGA-GBM.mutect2_snv.tsv','gProfiler_hsapiens_gbm.csv'],
-    'NBL':['TARGET-NBL.mutect2_snv.tsv','gProfiler_hsapiens_nbl.csv'],
-    'PAAD':['TCGA-PAAD.mutect2_snv.tsv','gProfiler_hsapiens_paad.csv'],
-    'HNSC':['TCGA-HNSC.mutect2_snv.tsv','gProfiler_hsapiens_hnsc.csv'],
-    'OV':['TCGA-OV.mutect2_snv.tsv','gProfiler_hsapiens_ov.csv'],
-    'LUSC':['TCGA-LUSC.somaticmutation_wxs.tsv','gProfiler_hsapiens_lusc.csv'],
-    'LUAD':['TCGA-LUAD.mutect2_snv.tsv','gProfiler_hsapiens_luad.csv'],
-    'CHOL':['TCGA-CHOL.somaticmutation_wxs.tsv','gProfiler_hsapiens_chol.csv'],
-    'SKCM':['TCGA-SKCM.mutect2_snv.tsv','gProfiler_hsapiens_skcm.csv'],
-    'ALL_BALL_P1':[None,None],
-    'KICH':['TCGA-KICH.somaticmutation_wxs.tsv','gProfiler_hsapiens_KICH.csv'],
-    'WT':['TARGET-WT.somaticmutation_wxs.tsv','gProfiler_hsapiens_wt.csv'],
-    'CCSK':[None,None],
-    'KIRP':['TCGA-KIRP.somaticmutation_wxs.tsv','gProfiler_hsapiens_KIRP.csv'],
-    'LGG':['TCGA-LGG.somaticmutation_wxs.tsv','gProfiler_hsapiens_LGG.csv'],
-    'READ':['TCGA-READ.somaticmutation_wxs.tsv','gProfiler_hsapiens_READ.csv'],
-    'UCS':['TCGA-UCS.somaticmutation_wxs.tsv','gProfiler_hsapiens_UCS.csv'],
-    'SARC':['TCGA-SARC.somaticmutation_wxs.tsv','gProfiler_hsapiens_sarc.csv'],
-    'TGCT':['TCGA-TGCT.somaticmutation_wxs.tsv','gProfiler_hsapiens_TGCT.csv'],
-    'THYM':['TCGA-THYM.somaticmutation_wxs.tsv','gProfiler_hsapiens_THYM.csv'],
-    'THCA':['TCGA-THCA.somaticmutation_wxs.tsv','gProfiler_hsapiens_thca.csv'],
-    'UVM':['TCGA-UVM.somaticmutation_wxs.tsv','gProfiler_hsapiens_uvm.csv'],
-    'OS':['TARGET-OS.mutect2_snv.tsv','gProfiler_hsapiens_os.csv'],
-    'PRAD':['TCGA-PRAD.mutect2_snv.tsv','gProfiler_hsapiens_prad.csv'],
-    'UCEC':['TCGA-UCEC.somaticmutation_wxs.tsv','gProfiler_hsapiens_ucec.csv']
+gene_dir = '/gpfs/data/yarmarkovichlab/Frank/pan_cancer/gene'
+custom_dic = {
+    'BRCA':['TCGA-BRCA.mutect2_snv.tsv','gProfiler_hsapiens_brca.csv','manifest_BRCA_1118.tsv','TCGA-BRCA.htseq_counts.tsv'],
+    'KIRC':['TCGA-KIRC.mutect2_snv.tsv','gProfiler_hsapiens_kirc.csv','manifest_kirc_542.tsv','TCGA-KIRC.htseq_counts.tsv'],
+    'COAD':['TCGA-COAD.mutect2_snv.tsv','gProfiler_hsapiens_coad.csv','manifest_coad_524.tsv','TCGA-COAD.htseq_counts.tsv'],
+    'STAD':['TCGA-STAD.somaticmutation_wxs.tsv','gProfiler_hsapiens_stad.csv','manifest_stad_448.tsv','TCGA-STAD.star_counts.tsv'],
+    'MESO':['TCGA-MESO.mutect2_snv.tsv','gProfiler_hsapiens_meso.csv','manifest_MESO_87.tsv','TCGA-MESO.htseq_counts.tsv'],
+    'LIHC':['TCGA-LIHC.mutect2_snv.tsv','gProfiler_hsapiens_lihc.csv','manifest_LIHC_424.tsv','TCGA-LIHC.htseq_counts.tsv'],
+    'ESCA':['TCGA-ESCA.somaticmutation_wxs.tsv','gProfiler_hsapiens_esca.csv','manifest_esca_198.tsv','TCGA-ESCA.star_counts.tsv'],
+    'CESC':['TCGA-CESC.mutect2_snv.tsv','gProfiler_hsapiens_cesc.csv','manifest_CESC_309.tsv','TCGA-CESC.htseq_counts.tsv'],
+    'BLCA':['TCGA-BLCA.somaticmutation_wxs.tsv','gProfiler_hsapiens_blca.csv','manifest_blca_431.tsv','TCGA-BLCA.star_counts.tsv'],
+    'RT':[None,None,'manifest_rt_69.tsv','TARGET-RT.star_counts.tsv'],
+    'AML':['TCGA-LAML.mutect2_snv.tsv','gProfiler_hsapiens_aml.csv','manifest_AML_151.tsv','TARGET-AML.star_counts.tsv'],
+    'DLBC':['TCGA-DLBC.mutect2_snv.tsv','gProfiler_hsapiens_dlbc.csv','manifest_DLBC_48.tsv','TCGA-DLBC.htseq_counts.tsv'],
+    'GBM':['TCGA-GBM.mutect2_snv.tsv','gProfiler_hsapiens_gbm.csv','manifest_GBM_175.tsv','TCGA-GBM.htseq_counts.tsv'],
+    'NBL':['TARGET-NBL.mutect2_snv.tsv','gProfiler_hsapiens_nbl.csv','manifest_NBL_161.tsv','TARGET-NBL.htseq_counts.tsv'],
+    'PAAD':['TCGA-PAAD.mutect2_snv.tsv','gProfiler_hsapiens_paad.csv','manifest_PAAD_183.tsv','TCGA-PAAD.htseq_counts.tsv'],
+    'HNSC':['TCGA-HNSC.mutect2_snv.tsv','gProfiler_hsapiens_hnsc.csv','manifest_HNSC_566.tsv','TCGA-HNSC.htseq_counts.tsv'],
+    'OV':['TCGA-OV.mutect2_snv.tsv','gProfiler_hsapiens_ov.csv','manifest_ov_429.tsv','TCGA-OV.htseq_counts.tsv'],
+    'LUSC':['TCGA-LUSC.somaticmutation_wxs.tsv','gProfiler_hsapiens_lusc.csv','manifest_lusc_553.tsv','TCGA-LUSC.star_counts.tsv'],
+    'LUAD':['TCGA-LUAD.mutect2_snv.tsv','gProfiler_hsapiens_luad.csv','manifest_LUAD_541.tsv','TCGA-LUAD.htseq_counts.tsv'],
+    'CHOL':['TCGA-CHOL.somaticmutation_wxs.tsv','gProfiler_hsapiens_chol.csv','manifest_chol_44.tsv','TCGA-CHOL.star_counts.tsv'],
+    'SKCM':['TCGA-SKCM.mutect2_snv.tsv','gProfiler_hsapiens_skcm.csv','manifest_SKCM_472.tsv','TCGA-SKCM.htseq_counts.tsv'],
+    'ALL_BALL_P1':[None,None,'manifest_all_ball_p1.tsv','TARGET-ALL-P1.star_counts.tsv'],
+    'KICH':['TCGA-KICH.somaticmutation_wxs.tsv','gProfiler_hsapiens_KICH.csv','manifest_KICH-91.tsv','TCGA-KICH.star_counts.tsv'],
+    'WT':['TARGET-WT.somaticmutation_wxs.tsv','gProfiler_hsapiens_wt.csv','manifest_WT_136.tsv','TARGET-WT.star_counts.tsv'],
+    'CCSK':[None,None,'manifest_ccsk_13.tsv','TARGET-CCSK.star_counts.tsv'],
+    'KIRP':['TCGA-KIRP.somaticmutation_wxs.tsv','gProfiler_hsapiens_KIRP.csv','manifest_KIRP-323.tsv','TCGA-KIRP.star_counts.tsv'],
+    'LGG':['TCGA-LGG.somaticmutation_wxs.tsv','gProfiler_hsapiens_LGG.csv','manifest_LGG_534.tsv','TCGA-LGG.star_counts.tsv'],
+    'READ':['TCGA-READ.somaticmutation_wxs.tsv','gProfiler_hsapiens_READ.csv','manifest_READ_177.tsv','TCGA-READ.star_counts.tsv'],
+    'UCS':['TCGA-UCS.somaticmutation_wxs.tsv','gProfiler_hsapiens_UCS.csv','manifest_UCS_57.tsv','TCGA-UCS.star_counts.tsv'],
+    'SARC':['TCGA-SARC.somaticmutation_wxs.tsv','gProfiler_hsapiens_sarc.csv','manifest_SARC_265.tsv','TCGA-SARC.star_counts.tsv'],
+    'TGCT':['TCGA-TGCT.somaticmutation_wxs.tsv','gProfiler_hsapiens_TGCT.csv','manifest_TGCT_156.tsv','TCGA-TGCT.star_counts.tsv'],
+    'THYM':['TCGA-THYM.somaticmutation_wxs.tsv','gProfiler_hsapiens_THYM.csv','manifest_THYM_122.tsv','TCGA-THYM.star_counts.tsv'],
+    'THCA':['TCGA-THCA.somaticmutation_wxs.tsv','gProfiler_hsapiens_thca.csv','manifest_THCA_572.tsv','TCGA-THCA.star_counts.tsv'],
+    'UVM':['TCGA-UVM.somaticmutation_wxs.tsv','gProfiler_hsapiens_uvm.csv','manifest_UVM_80.tsv','TCGA-UVM.star_counts.tsv'],
+    'OS':['TARGET-OS.mutect2_snv.tsv','gProfiler_hsapiens_os.csv','manifest_OS_88.tsv','TARGET-OS.htseq_counts.tsv'],
+    'PRAD':['TCGA-PRAD.mutect2_snv.tsv','gProfiler_hsapiens_prad.csv','manifest_PRAD_554.tsv','TCGA-PRAD.htseq_counts.tsv'],
+    'UCEC':['TCGA-UCEC.somaticmutation_wxs.tsv','gProfiler_hsapiens_ucec.csv','manifest_UCEC_589.tsv','TCGA-UCEC.star_counts.tsv']
 }
 
 root_des_dir = '/gpfs/data/yarmarkovichlab/public/ImmunoVerse/molecular_catalogue'
@@ -267,30 +268,34 @@ for c in cancers + added_cancer:
     splicing_rec = os.path.join(atlas_dir,'splicing_rec.txt')
     fusion_all = os.path.join(atlas_dir,'fusion.txt')
     fusion_recurrent = os.path.join(atlas_dir,'fusion_recurrent.txt')
+    te_all = os.path.join(atlas_dir,'tumor_erv.h5ad')
+    te_rec = os.path.join(atlas_dir,'ERV.txt')
+    te_good = os.path.join(atlas_dir,'good_erv.txt')
+    manifest = os.path.join(atlas_dir,custom_dic[c][2])
+    gene_count = os.path.join(gene_dir,custom_dic[c][3])
     real_common = os.path.join(atlas_dir,'real_common.txt')
     real_common_membrane = os.path.join(atlas_dir,'real_common_membrane.txt')
     if not c in ['NBL','OS','RT','CCSK','WT','ALL_BALL_P1']:
         hla_types = os.path.join(atlas_dir,'hla_types.txt')
+        manifest_fusion = None
     else:
         hla_types = None
+        manifest_fusion = os.path.join(atlas_dir,'manifest_fusion.tsv')
     if not c in ['NBL','OS','RT','CCSK','WT','ALL_BALL_P1','CHOL','MESO','UVM','SARC']:
         deg_file = os.path.join(atlas_dir,'deg.txt')
     else:
         deg_file = None
-    if variant_dic[c][0] is None:
+    if custom_dic[c][0] is None:
         raw_mutation = None
         map_mutation = None
         mutation_rec = None
     else:
-        raw_mutation = os.path.join(variant_dir,variant_dic[c][0])
-        map_mutation = os.path.join(variant_dir,variant_dic[c][1])
+        raw_mutation = os.path.join(variant_dir,custom_dic[c][0])
+        map_mutation = os.path.join(variant_dir,custom_dic[c][1])
         mutation_rec = os.path.join(atlas_dir,'mutation_rec.txt')
-    te_all = os.path.join(atlas_dir,'tumor_erv.h5ad')
-    te_rec = os.path.join(atlas_dir,'ERV.txt')
-    te_good = os.path.join(atlas_dir,'good_erv.txt')
 
     need_to_cp = [gene_tpm,gene_lfc,pathogen_all,pathogen_rec,intron_all,intron_peptide_all,intron_rec,splicing_all,splicing_rec,
-                  fusion_all,fusion_recurrent, real_common, real_common_membrane, hla_types, deg_file, mutation_rec, raw_mutation, map_mutation, te_all, te_rec, te_good]
+                  fusion_all,fusion_recurrent, manifest,gene_count, real_common, real_common_membrane, hla_types, manifest_fusion, deg_file, mutation_rec, raw_mutation, map_mutation, te_all, te_rec, te_good]
 
     des_dir = os.path.join(root_des_dir,c)
     if not os.path.exists(des_dir):
@@ -299,7 +304,6 @@ for c in cancers + added_cancer:
     for f in need_to_cp:
         if f is not None:
             subprocess.run('cp {} {}'.format(f,des_dir),shell=True)
-
 
 # search space tesorai
 root_des_dir = '/gpfs/data/yarmarkovichlab/public/ImmunoVerse/search_space_tesorai'
